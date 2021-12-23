e07cb0111d85905e45b7770e702e1e048f591274
olena <melolena33@gmail.com>
2021-12-23 19:15:06 +0200

    Refesh knowledge

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const spans = document.querySelectorAll('.population');
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+
+const populations = [...spans].map(span => +span.textContent.replace(/,/g, ''));
+const totalPopulation = populations.reduce((sum, amount) => sum + amount, 0);
+
+total.textContent = totalPopulation.toLocaleString();
+average.textContent = (totalPopulation / populations.length).toLocaleString();
